10e87257cdc677ea106285efcce32c556f442778
StakLabs <staklabsofficial@gmail.com>
2026-07-05 15:01:23 +1000

    Add files via upload

---
+++ api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+const databaseName = 'sql12832223';
+const db = mysql.createConnection({
+    host: 'sql12.freesqldatabase.com',
+    port: 3306,
+    user: 'sql12832223',
+    password: 'wARtFkTpXe',
+    database: databaseName
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+app.get('/', (req, res) => {
+    res.send('API is working');
+});
